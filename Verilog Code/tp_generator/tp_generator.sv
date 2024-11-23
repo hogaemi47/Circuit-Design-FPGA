7d34d8904ee5017e24cc05614c97a5e3568447b3
hogaemi47 <rlaghrud1234@hanyang.ac.kr>
2024-11-24 00:40:10 +0900

    commit

--- Verilog Code/tp_generator/tp_generator.sv
+++ Verilog Code/tp_generator/tp_generator.sv
@@ -85,7 +85,7 @@ module tp_generator (
 		else if (h_cnt < HSP) begin
 			h_line <= 0;
 		end
-		else if (h_cnt >= HSP && h_cnt < HWIDTH) begin
+		else if (h_cnt >= HSP && h_cnt < (HWIDTH - HFP)) begin
 			h_line <= 1;
 		end
 		else begin
@@ -139,7 +139,7 @@ module tp_generator (
 		if (sys_rst) begin
 			line_cnt <= 0;
 		end
-		else if (h_line & ~h_line_d) begin
+		else if (vsync && (h_line & ~h_line_d)) begin
 			line_cnt <= line_cnt + 1;
 		end
 		else if (line_cnt == VWIDTH) begin
